bec2c69bbdef05ccd78267fc48656fd9229fe128
Paul Bauknecht <paul_bauknecht@gmx.de>
2017-07-21 23:41:29 +0200

    Changed access to std::vector-members in Cell such that .at() is used instead of operator[]. The benefit is a range check.

--- src/Cell.cc
+++ src/Cell.cc
@@ -12,18 +12,18 @@ void Cell::AddBody(Vector3d posArg, Vector3d velArg, Vector3d extForceArg) {
 }
 
 void Cell::CreateBond(const int id1, const int id2) {
-	auto body1 = bodies_[id1];
-	auto body2 = bodies_[id2];
+	auto body1 = bodies_.at(id1);
+	auto body2 = bodies_.at(id2);
 
-	Bond newBond(bodies_[id1], bodies_[id2]);
+	Bond newBond(bodies_.at(id1), bodies_.at(id2));
 	bonds_.push_back(newBond);
 }
 
 void Cell::CreateBond(const int id1, const int id2, const double eqDist, const double strength) {
-	auto body1 = bodies_[id1];
-	auto body2 = bodies_[id2];
+	auto body1 = bodies_.at(id1);
+	auto body2 = bodies_.at(id2);
 
-	Bond newBond(bodies_[id1], bodies_[id2], eqDist, strength);
+	Bond newBond(bodies_.at(id1), bodies_.at(id2), eqDist, strength);
 	bonds_.push_back(newBond);
 }
 
